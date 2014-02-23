045ce57b4a8233414375ff73fac807ca8949c3b1
mmhelloworld <mmhelloworld@gmail.com>
2014-02-23 04:51:32 -0500

    Refactor

--- try-frege-web/src/main/java/tryfrege/web/ContextListener.java
+++ try-frege-web/src/main/java/tryfrege/web/ContextListener.java
@@ -1,127 +1,75 @@
 package tryfrege.web;
 
-import java.io.InputStream;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Scanner;
+import frege.interpreter.FregeInterpreter;
+import frege.interpreter.javasupport.InterpreterClassLoader;
+import frege.prelude.PreludeBase;
+import frege.runtime.Lambda;
 
-import javax.script.ScriptContext;
-import javax.script.SimpleScriptContext;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
+import java.io.InputStream;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
 
-import frege.prelude.PreludeBase.TEither;
-import frege.prelude.PreludeBase.TList;
-import frege.prelude.PreludeBase.TList.DCons;
-import frege.prelude.PreludeBase.TMaybe;
-import frege.runtime.Lambda;
-import frege.runtime.Lazy;
-import frege.scriptengine.FregeScriptEngine;
+import static frege.scriptengine.FregeScriptEngine.toJavaValue;
 
 public class ContextListener implements ServletContextListener {
 
-  @Override
-  public void contextDestroyed(final ServletContextEvent arg0) {
+    @Override
+    public void contextDestroyed(final ServletContextEvent arg0) {
 
-  }
-
-  @Override
-  public void contextInitialized(final ServletContextEvent event) {
-    final ServletContext context = event.getServletContext();
-    context.setAttribute("preludeScripts", preludeScripts(context));
-    evalPrelude(preludeScripts(context).get(0), context);
-  }
-
-  private void evalPrelude(final String prelude, final ServletContext context) {
-    final SimpleScriptContext scontext = new SimpleScriptContext();
-    final Lambda io = FregeScriptEngine.load(prelude, scontext);
-    final TEither intpRes = io.apply(1).result().<TEither> forced();
-    final int cons = intpRes._constructor();
-    if (cons == 0) {
-      final TList errs = getLeft(intpRes);
-      final List<String> errMsgs = toJavaList(errs);
-      context.log(errMsgs.toString());
-    } else {
-      context.setAttribute("classes", scontext.getAttribute("classes"));
-    }
-  }
-
-  public static <A> A getLeft(final TEither either) {
-    final TEither.DLeft left = (TEither.DLeft) either._Left().call();
-    final A result;
-    if (left.mem1 instanceof Lazy) {
-      result = ((Lazy) left.mem1).forced();
-    } else {
-      result = (A) left.mem1;
-    }
-    return result;
-  }
-
-  public static <A> A getRightMaybe(final TEither either) {
-    final TEither.DRight right = (TEither.DRight) either._Right().call();
-    final A result;
-    if (right.mem1 instanceof Lazy) {
-      final TMaybe valueMaybe = ((Lazy) right.mem1).<TMaybe> forced();
-      result = toJavaValue(valueMaybe);
-    } else {
-      result = toJavaValue((TMaybe) right.mem1);
-    }
-    return result;
-  }
-
-  public static <A> A getRight(final TEither either) {
-    final TEither.DRight right = (TEither.DRight) either._Right().call();
-    final A result;
-    if (right.mem1 instanceof Lazy) {
-      result = ((Lazy) right.mem1).forced();
-    } else {
-      result = (A) right.mem1;
-    }
-    return result;
-  }
-
-  private static <A> A toJavaValue(final TMaybe valueMaybe) {
-    final A result;
-    if (valueMaybe._constructor() == 0) {
-      result = null;
-    } else {
-      result = (A) valueMaybe._Just().mem1;
-    }
-    return result;
-  }
-
-  private static <A> List<A> toJavaListLoop(final TList list, final List<A> acc) {
-    if (list._constructor() == 0) { // Nil
-      return acc;
-    } else {
-      final DCons cons = list._Cons();
-      final A elem;
-      if (cons.mem1 instanceof Lazy) {
-        elem = ((Lazy) cons.mem1).<A> forced();
-      } else {
-        elem = (A) cons.mem1;
-      }
-      acc.add(elem);
-      return toJavaListLoop((TList) cons.mem2.forced(), acc);
-    }
-  }
-
-  public static <A> List<A> toJavaList(final TList list) {
-    final List<A> jlist = toJavaListLoop(list, new ArrayList<A>());
-    return Collections.unmodifiableList(jlist);
-  }
-
-  private List<String> preludeScripts(final ServletContext context) {
-    final InputStream ioScriptStream = context
-        .getResourceAsStream("/WEB-INF/fregescripts/PreludeScripting.fr");
-    try (final Scanner scanner = new Scanner(ioScriptStream)) {
-      final String preludeScript = scanner.useDelimiter("\\Z").next();
-      return Arrays.asList(preludeScript);
     }
 
-  }
+    @Override
+    public void contextInitialized(final ServletContextEvent event) {
+        final ServletContext context = event.getServletContext();
+        context.setAttribute("preludeScripts", preludeScripts(context));
+        evalPrelude(preludeScripts(context).get(0), context);
+    }
+
+    private void evalPrelude(final String prelude, final ServletContext context) {
+        final Lambda res = FregeInterpreter.interpret(prelude);
+        final FregeInterpreter.TInterpreterConfig config = toJavaValue(FregeInterpreter.TInterpreterConfig._default);
+        final FregeInterpreter.TInterpreterClassLoader classLoader =
+            toJavaValue(FregeInterpreter.TInterpreterClassLoader._default);
+        final PreludeBase.TTuple2 intpRes = FregeInterpreter.TInterpreter.run(
+            res, config, classLoader).forced();
+        context.setAttribute("classes", classes(intpRes));
+    }
+
+    private Object classes(final PreludeBase.TTuple2 tup) {
+        final FregeInterpreter.TInterpreterResult interpRes = toJavaValue(tup.mem1);
+        final InterpreterClassLoader classLoader = toJavaValue(tup.mem2);
+        final Map<String, byte[]> classes;
+        switch (interpRes._constructor()) {
+            case 0: //Success
+                FregeInterpreter.TInterpreterResult.DSuccess success = interpRes._Success();
+                final FregeInterpreter.TSourceInfo srcinfo = toJavaValue(success.mem$sourceRepr);
+                if (srcinfo._constructor() == 0) { // Module
+                    classes = classLoader.classes();
+                } else {
+                    classes = new HashMap<>();
+                }
+                break;
+            default:
+                classes = new HashMap<>();
+                break;
+        }
+        return classes;
+    }
+
+    private List<String> preludeScripts(final ServletContext context) {
+        final InputStream ioScriptStream = context
+            .getResourceAsStream("/WEB-INF/fregescripts/PreludeScripting.fr");
+        try (final Scanner scanner = new Scanner(ioScriptStream)) {
+            final String preludeScript = scanner.useDelimiter("\\Z").next();
+            return Arrays.asList(preludeScript);
+        }
+
+    }
 
 }
--- try-frege-web/src/main/webapp/WEB-INF/pages/replResponse.jsp
+++ try-frege-web/src/main/webapp/WEB-INF/pages/replResponse.jsp
@@ -2,23 +2,8 @@
   pageEncoding="UTF-8"%>
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
 <repl>
-  <type>${msgType}</type>
 
-  <c:choose>
-  <c:when test="${msgType == 'ERROR'}">
-    <message><![CDATA[${msg}]]></message>
-  </c:when>
-  <c:when test="${msgType == 'MESSAGE'}">
-    <message><![CDATA[${msg}]]></message>
-  </c:when>
-  <c:when test="${msgType == 'SUCCESS'}">
-    <message><![CDATA[${msg}]]></message>
-    <out>${out}</out>
-    <err>${err}</err>
-  </c:when>
-  <c:otherwise>
-    <message></message>
-  </c:otherwise>
-
-  </c:choose>
+  <messageType></messageType>
+  <position></position>
+  <message></message>
 </repl>
